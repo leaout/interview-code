cb8e377efa71e98c4a113343c4bb764ac7263c02
chenliangyu <chenliangyu@ultipa.com>
2023-11-17 20:15:10 +0800

    update

--- main.cpp
+++ main.cpp
@@ -291,8 +291,88 @@ void test_find_max_path() {
     s1.find_max_path(matr, 5, 5);
 
 }
+/*
+ * 给出一个字符串 S，考虑其所有重复子串（S 的连续子串，出现两次或多次，可能会有重叠）。
+    返回任何具有最长可能长度的重复子串。（如果 S 不含重复子串，那么答案为 ""。）
+ * */
+class SolLonggestSubString {
+public:
+    //检查是针对存在重复子串 还是发生了哈希冲突
+    bool check_hash(string& s, pair<int, int>& a, pair<int, int> b) {       //查看是否是真重复子串还是因为发生哈希碰撞而导致哈希值相同
+        for (int i = a.first, j = b.first; i != a.second && j != b.second; ++i, ++j) {
+            if (s[i] != s[j]) return false;
+        }
+        return true;
+    }
+    //检查字符串s中是否存在长度为len的重复子串，如果有则返回该子串，否则返回空字符串
+    string check(string& s, int len){
+        int base = 26;//二十六个字母对应二十六进制
+        int mod = 1000007;//取模 避免哈希冲突
 
+        int num = 0;
+        for(int i = 0; i < len; i++)//计算出第一个len长度的哈希映射值
+            num = (num * base + s[i] - 'a')%mod;
+
+        unordered_map<int, pair<int, int>> seen;//存储的是哈希映射值及对应的坐标
+        seen.insert({num, {0, len - 1}});
+
+        int al = 1;//根据公式 计算出常数a的len次方
+        for(int i = 1; i <= len; i++)
+            al = (al * base)%mod;
+
+        //遍历字符串 计算每一个长度为len的子串的哈希映射值
+        for(int i = 1; i < s.size() - len + 1; i++){
+
+            //计算长度为len的子串的哈希映射值
+            num = num * base - ((s[i-1] - 'a') * al)%mod;
+            while(num < 0) num += mod;
+            num = (num + (s[i + len - 1] - 'a'))%mod;
+
+            //查找是否有重复的子串
+            if(seen.count(num))
+                if(check_hash(s, seen[num], {i, i + len - 1}))
+                    return s.substr(i, len);//如果是真的存在而不是因为哈希冲突，就返回这个子串
+            seen.insert({num, {i, i + len - 1}});//如果是哈希冲突 就插入
+        }
+        return "";
+    }
+
+    //返回字符串s最长重复子串
+    string longestDupSubstring(string s) {
+        int m = s.size();
+        int left = 0, right = m;
+        string res = "";
+        while(left < right){
+            int mid = left + (right - left)/2;//二分法找到最长重复子串
+            string tmp = check(s, mid);
+            if(!tmp.empty()){//如果存在重复子串，就保存下来最长的一个重复子串
+                res = tmp.size() > res.size() ? tmp : res;
+                left = mid + 1;
+            }else
+                right = mid;
+
+        }
+        return res;
+    }
+};
+
+/*
+ * 一个自重复串是一个字符串，其前一半和后一半是一样的，例如 abcdbabcdb （长度一定是偶数）。
+    输入一个字符串，找出其中最长的自重复子串。这里的子串要求连续。
+
+    例子
+    输入：
+    abababcdabcd
+    输出：
+    abcdabcd
+
+
+    测试输入：faaacabcddcbabcddcbedfgaac
+ *
+ * */
 int main() {
+    SolLonggestSubString sls;
+    std ::cout << "result:" << sls.longestDupSubstring("faaacabcddcbabcddcbedfgaac") << std::endl;
     return 0;
 }
 
